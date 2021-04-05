3cfd58a99db56959c8391eeb16e8b7934929084f
Shruthic27 <76080986+Shruthic27@users.noreply.github.com>
2021-04-05 14:59:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var name="Shruthi"
+var lname="Chava"
+var num1="6"
+var num2="3"
+var num3="9"
+console.log(name.concat(lname))
+console.log("The min is"+Math.min(num1,num2))
+console.log("The max is"+Math.max(num1,num2))
+console.log("6-3="+(num1-num2))
+console.log("6+3="+(6+3))
+console.log("6*3="+(num1*num2))
+console.log("6÷3="+(num1/num2))
+console.log("6^3="+Math.pow(num1,num2))
+console.log("The square root of 6 is"+Math.sqrt(num1))
